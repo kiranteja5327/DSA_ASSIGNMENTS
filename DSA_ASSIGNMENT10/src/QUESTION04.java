552b772ca20e4be5ab9c876ffcc000a9c63274f0
kiranteja5327 <115568222+kiranteja5327@users.noreply.github.com>
2023-06-26 21:49:08 +0530

    Add files via upload

---
+++ DSA_ASSIGNMENT10/src/QUESTION04.java
@@ -0,0 +1,30 @@
+
+/*
+Given a string calculate length of the string using recursion.
+
+**Examples:**
+*Input : str = "abcd"
+Output :4
+
+Input : str = "GEEKSFORGEEKS"
+Output :13
+*/
+
+
+
+public class QUESTION04 {
+
+	public static int calculateLength(String str) {
+        if (str.isEmpty()) {
+            return 0;
+        }
+        return 1 + calculateLength(str.substring(1));
+    }
+
+    public static void main(String[] args) {
+        String str = "GEEKSFORGEEKS";
+        int length = calculateLength(str);
+        System.out.println("Length of the string: " + length);
+    }
+   
+}
